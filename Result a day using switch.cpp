52a026b1a8cca0da797b589141f41dc1c6fdfec0
ZAIN IQBAL <124054465+Zaini04@users.noreply.github.com>
2023-02-09 21:00:37 +0500

    Add files via upload

---
+++ Result a day using switch.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int day=8;
+	switch(day)
+	{
+	
+	case 1:
+		cout<<"Sunday";
+		
+		break;
+		
+		case 2:
+		cout<<"Monday";
+		
+		break;
+	    case 3:
+	    cout<<"Tuesday";
+	    break;
+	    case 4:
+	    	cout<<"wednesday";
+	    	break;
+	    	case 5:
+	    		cout<<"Thursday";
+	    		break;
+	    		case 6:
+	    			cout<<"Friday";
+	    			break;
+	    			case 7:
+	    				cout<<"Saturday";
+	    				break;
+	    				default:
+	    				cout<<"No Day";
+	}
+		return 0;
+}
